5c2d73c436f0b75eac68554ccfd41bd22149832b
KoalaV2 <theo.johanson@gmail.com>
2021-04-24 18:28:55 +0200

    yes

--- main.py
+++ main.py
@@ -6,24 +6,37 @@ import json
 
 with open("products.json") as f:
     try:products = json.load(f)
-    except:products = {}
+    except Exception as e: print(e)
 
 def getproductid():
+    productid = []
     for i in products['hak5_products']:
-        productid = i["id"]
-        print(productid)
+        productid.append(i["id"])
+    return(productid)
+
+def getproductname():
+    productname = []
+    for i in products['hak5_products']:
+        productname.append(i["name"])
+    return(productname)
 
 def pineapple():
-    url = 'https://hak5.passportshipping.com/'
-    response = requests.get(url)  # Makes a request to the url and downloads plain html
-    soup = BeautifulSoup(response.text,"html.parser")
-    status_div = soup.find_all("div", {"class": "message"})[0] # Finds the div which has the status info
-    delivery_div = soup.find_all("div",{"class": "date range"} )
-    print(delivery_div)
-    print(f"Pineapple: {status_div}")
+    productlist = getproductid()
+    productname = getproductname()
+
+    for i in productlist:
+        url = f'https://hak5.passportshipping.com/{i}'
+
+        response = requests.get(url)  # Makes a request to the url and downloads plain html
+        soup = BeautifulSoup(response.text,"html.parser")
+        status_div = soup.find_all("div", {"class": "message"})[0] # Finds the div which has the status info
+        delivery_div = soup.find_all("div",{"class": "date range"} )
+        print(delivery_div)
+        print(f"{productname}: {status_div}")
 
 def main():
-    getproductid()
+    #getproductid()
+    pineapple()
 
 if __name__=="__main__":
     main()
