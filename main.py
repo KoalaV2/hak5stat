d3431bfa9d1f8d6b0f748e62a002de6009596b8d
KoalaV2 <theo.johanson@gmail.com>
2021-04-25 16:00:54 +0200

    removed unused functions and made output nicer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import requests
-import urllib3.request
 from bs4 import BeautifulSoup
 import json
 
@@ -8,28 +7,16 @@ with open("products.json") as f:
     try:products = json.load(f)
     except Exception as e: print(e)
 
-def getproductid():
-    productid = []
-    for i in products['hak5_products']:
-        productid.append(i["id"])
-    return(productid)
-
-def getproductname():
-    productname = []
-    for i in products['hak5_products']:
-        productname.append(i["name"])
-    return(productname)
-
 def get_pairs():
     lst_of_pairs = []
     for product in products['hak5_products']:
         pair = (product['name'],product['id'])
         lst_of_pairs.append(pair)
     return lst_of_pairs
+
 def getstatus():
  
     for (name,id) in get_pairs():
-        print(f"{name} : {id}")
 
         url = f'https://hak5.passportshipping.com/{id}'
         response = requests.get(url)  # Makes a request to the url and downloads plain html
@@ -37,11 +24,10 @@ def getstatus():
 
         status_div = soup.find_all("div", {"class": "message"})[0] # Finds the div which has the status info
         delivery_div = soup.find_all("div",{"class": "date range"} )
-        print(delivery_div)
+        print(f"{name} : {delivery_div}")
         print(f"{name}: {status_div}")
 
 def main():
-    #getproductid()
     getstatus()
 
 if __name__=="__main__":
